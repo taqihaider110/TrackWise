e0d489ab3662f40789d308b3f442c3dbeec74eea
Muhammad Taqi Haider <taqi.haider@koderlabs.com>
2025-05-11 20:19:04 +0500

    Will Work on every server

--- BACKEND/server.js
+++ BACKEND/server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 require('dotenv').config(); // Load environment variables
-const open = require('open').default; // To open Swagger UI in browser
+// const open = require('open').default; // To open Swagger UI in browser
 
 const { connectDB } = require('./config/db'); // DB connection
 
